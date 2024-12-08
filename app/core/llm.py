803a7ce79e66d31ce020a4fab6ca889a1a9fbab3
javierbuilder <javierbuilder@protonmail.ch>
2024-12-08 15:31:26 -0300

    configuracion de cohere y creacion del prompt

---
+++ app/core/llm.py
@@ -0,0 +1,21 @@
+import cohere
+from app.core.config import COHERE_API_KEY
+
+cohere_client = cohere.Client(COHERE_API_KEY)
+
+
+def build_prompt(question, context):
+	return (f"Context: {context}\n"
+		f"Question: {question}\n"
+		f"Answer in one sentence, in  third person, in  the same language, and with emojis.")
+
+def  ask_llm(prompt):
+	response = cohere_client.generate(
+		model ="command-xlarge-nightly",
+		prompt=prompt,
+		max_tokens = 50,
+		temperature= 0
+	)
+	return response.generations[0].text.strip()
+
+
