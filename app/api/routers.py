6aadf3254c14b3ad33fc6ef7222378dd54e5dec3
javierbuilder <javierbuilder@protonmail.ch>
2024-12-08 14:59:00 -0300

    defino los endpoints de la api fastapi

---
+++ app/api/routers.py
@@ -0,0 +1,19 @@
+from  fastapi import APIRouter
+from app.models.document import DocumentRequest
+from app.models.question import QuestionRequest, AnswerResponse
+from app.core.embeddings import store_embeddings, retrieve_context
+from app.core.llm import ask_llm, build_prompt
+
+router = APIRouter()
+
+@router.post("/add-document")
+def add_document(request: DocumentRequest):
+	store_embeddings(request.document_name, request.content)
+	return {"message":"Documento procesado y guardado correctamente."}
+
+@router.post("/ask", response_model=AnswerResponse)
+	context = retrieve_context(request.question)
+	prompt =  build_prompt(request.question, context)
+	answer = ask_llm(prompt)
+	return AnswerResponse(user_name=request.user_name, question=request.question, answer=answer)
+
